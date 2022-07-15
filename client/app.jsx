20f0be7eb254644890cc0db74601d4400fcf5b06
Michael Moncera <michaelmoncera@gmail.com>
2022-07-15 03:19:07 +0000

    Created page-container component. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Header from './components/header';
+import PageContainer from './components/page-container';
+import Home from './pages/home';
 
 function App() {
   return (
     <>
       <Header/>
+      <PageContainer >
+        <Home />
+        </ PageContainer>
     </>
   );
 }
