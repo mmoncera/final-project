039be8cc1f92c704f894fe35afba847a28f91780
Michael Moncera <michaelmoncera@gmail.com>
2022-07-21 23:40:41 +0000

    Designed Home component page. #4

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -9,7 +9,21 @@ function Home() {
     return <Redirect to="sign-in" />;
   }
 
-  return <h1>React Home</h1>;
+  return (
+    <div className="row justify-content-center home-container-padding">
+      <div className="col-sm-10 col-md-9 col-lg-7">
+        <h1 className="text-center mb-4 font-rubik">Find an Event</h1>
+        <form>
+          <div className="input-group">
+            <input type="text" className="form-control" placeholder="food, drinks, fun..." aria-label="Username" />
+            <input type="text" className="form-control" placeholder="location" aria-label="Server" />
+            <button className="btn btn-primary"><i className="fa-solid fa-magnifying-glass"></i></button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+
 }
 
 export default Home;
