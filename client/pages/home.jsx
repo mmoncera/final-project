e5340e1e0458e0b049fe48930d6f3ece7764b43e
Michael Moncera <michaelmoncera@gmail.com>
2022-07-22 20:44:11 +0000

    Updated Home component with search bar and appropriate handlers. #4

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -1,9 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import Redirect from '../components/redirect';
 import { AppContext } from '../lib';
 
 function Home() {
+  const [searchInfo, setSearchInfo] = useState({ term: '', location: '' });
   const { user } = useContext(AppContext);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setSearchInfo({ ...searchInfo, [name]: value });
+  }
+
+  function handleSearch(event) {
+    event.preventDefault();
+    window.location.hash = `#result?term=${searchInfo.term}&location=${searchInfo.location}`;
+  }
 
   if (!user) {
     return <Redirect to="sign-in" />;
@@ -13,17 +24,33 @@ function Home() {
     <div className="row justify-content-center home-container-padding">
       <div className="col-sm-10 col-md-9 col-lg-7">
         <h1 className="text-center mb-4 font-rubik">Find an Event</h1>
-        <form>
+        <form onSubmit={handleSearch}>
           <div className="input-group">
-            <input type="text" className="form-control" placeholder="food, drinks, fun..." aria-label="Username" />
-            <input type="text" className="form-control" placeholder="location" aria-label="Server" />
-            <button className="btn btn-primary"><i className="fa-solid fa-magnifying-glass"></i></button>
+            <input
+            className="form-control bg-light"
+            type="text"
+            name="term"
+            value={searchInfo.term}
+            placeholder="food, drinks, fun..."
+            required
+            autoFocus
+            onChange={handleChange} />
+            <input
+            className="form-control bg-light"
+            type="text"
+            name="location"
+            value={searchInfo.location}
+            placeholder="location"
+            required
+            onChange={handleChange} />
+            <button className="btn btn-primary" type="submit"><i className="fa-solid fa-magnifying-glass"></i></button>
           </div>
         </form>
       </div>
     </div>
   );
-
 }
 
 export default Home;
+
+// clean up
